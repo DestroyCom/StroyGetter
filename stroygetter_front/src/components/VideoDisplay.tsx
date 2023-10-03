9300a6a481a3e9c3001830f3950ddfca9a9695aa
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-10-03 03:27:01 +0200

    feat: add fake loading state

--- stroygetter_front/src/components/VideoDisplay.tsx
+++ stroygetter_front/src/components/VideoDisplay.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { axios_intcs } from '../lib/axios';
 import clsx from 'clsx';
+import { Progress } from './ui/progress';
 
 export const VideoDisplay = ({
   url,
@@ -21,6 +22,7 @@ export const VideoDisplay = ({
     itag: string;
   }[];
 }) => {
+  const [loadProgress, setLoadProgress] = useState(0);
   const [chooseFormat, setChooseFormat] = useState('best');
 
   const getVideo = useQuery({
@@ -73,6 +75,37 @@ export const VideoDisplay = ({
     });
   }, [formats]);
 
+  useEffect(() => {
+    if (getVideo.isFetching) {
+      const maxProgress = 90;
+      const incrementDuration = 10000;
+      const incrementInterval = 25;
+
+      const startTime = Date.now();
+
+      const updateProgress = () => {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
+
+        const newProgress = maxProgress * (1 - Math.exp(-elapsedTime / incrementDuration));
+
+        setLoadProgress(newProgress);
+
+        if (newProgress >= maxProgress) {
+          clearInterval(interval);
+        }
+      };
+
+      updateProgress();
+
+      const interval = setInterval(updateProgress, incrementInterval);
+
+      return () => clearInterval(interval);
+    }
+
+    return undefined;
+  }, [getVideo.isFetching]);
+
   return (
     <section className="py-8">
       <div className="mx-auto my-2 flex h-auto w-11/12 rounded-lg border-2 border-dashed border-primary py-2 md:py-4 lg:text-xl">
@@ -99,14 +132,27 @@ export const VideoDisplay = ({
                 <SelectValue placeholder="Quality" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="best">Best quality</SelectItem>
+                <SelectItem value="best">
+                  <p className="flex justify-between">
+                    <span className="my-auto">Best quality</span>{' '}
+                    {getVideo.isFetching && <Loader2 className="ml-2 animate-spin md:hidden" size={24} />}
+                  </p>
+                </SelectItem>
                 {formats &&
                   formats.map((format) => (
                     <SelectItem key={format.qualityLabel} value={format.itag}>
-                      {format.qualityLabel}
+                      <p className="flex justify-between">
+                        <span className="my-auto">{format.qualityLabel}</span>{' '}
+                        {getVideo.isFetching && <Loader2 className="ml-2 animate-spin md:hidden" size={24} />}
+                      </p>
                     </SelectItem>
                   ))}
-                <SelectItem value="music">Music</SelectItem>
+                <SelectItem value="music">
+                  <p className="flex justify-between">
+                    <span className="my-auto">Music</span>{' '}
+                    {getVideo.isFetching && <Loader2 className="ml-2 animate-spin md:hidden" size={24} />}
+                  </p>
+                </SelectItem>
               </SelectContent>
             </Select>
             <button
@@ -120,7 +166,7 @@ export const VideoDisplay = ({
                 'flex w-full flex-row justify-center rounded-lg border-2 border-transparent bg-[#102F42] px-4 py-2 text-center font-bold text-white transition-all ease-in-out',
                 'md:mx-2',
                 !getVideo.isFetching && 'hover:cursor-pointer hover:border-primary hover:bg-secondary',
-                getVideo.isFetching && 'opacity-50',
+                getVideo.isFetching && 'hidden opacity-50 md:flex',
               )}
             >
               {!getVideo.isFetching && getVideo.isSuccess && (
@@ -141,6 +187,11 @@ export const VideoDisplay = ({
               )}
             </button>
           </div>
+          {getVideo.isFetching && (
+            <div className={clsx('mx-2 my-auto flex h-10 flex-col justify-end', 'md:my-2 md:flex-row')}>
+              <Progress value={loadProgress} className="my-auto" />
+            </div>
+          )}
         </div>
       </div>
 
